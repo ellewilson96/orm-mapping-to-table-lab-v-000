6b9aa57a54fc03e31f6139a209501443aa1709e4
Elle Wilson <eleanorwilsonn@gmail.com>
2018-04-16 17:25:52 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,5 +7,17 @@ class Student
   def initialize(id: nil, name:, grade:)
   end
 
+  def self.create_table 
+  end 
 
+  def self.drop_table
+  end 
+
+  def self.create 
+  end
+
+  def self.save 
+  end
+
+  
 end
