53d9ae5b318bef7690486b683d292cc0425c6241
Elle Wilson <eleanorwilsonn@gmail.com>
2018-04-16 17:30:41 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,7 +24,7 @@ class Student
   def self.create
   end
 
-  def self.save
+  def self.save(name, grade)
     DB[:conn].execute("INSERT INTO students (name, grade) VALUES (?, ?)", name, grade)
   end
 
