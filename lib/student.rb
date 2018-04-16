2c7371ab686a226f5f78016e1c85560fc9aadf67
Elle Wilson <eleanorwilsonn@gmail.com>
2018-04-16 17:53:36 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,12 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
+  def initialize(name, grade, id=nil)
+  @id = id
+  @name = name
+  @grade = grade
+  end
+
   def self.create_table
     sql =  <<-SQL
       CREATE TABLE IF NOT EXISTS students (
@@ -19,7 +25,7 @@ class Student
     DB[:conn].execute("DROP TABLE students;")
   end
 
-  def self.create(name, grade)
+  def self.create(name:, grade:)
     student = Student.new(name, grade)
     student.save
     student
